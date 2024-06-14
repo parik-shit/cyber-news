d04fd0cd3e967fe20776c56daad1aaaaa057998b
parik-shit <62713994+parik-shit@users.noreply.github.com>
2024-06-14 10:14:53 +0530

    added theme button

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import { NavbarDemo } from "./components/navbar";
+import { ModeToggle } from "./components/toggle-button";
+import { ThemeProvider } from "@/components/ui/theme-provider"
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -15,8 +17,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en"  suppressHydrationWarning>
+      <body className={inter.className}>
+        <header>
+          <NavbarDemo />
+        </header>
+        <ThemeProvider  attribute="class"
+            defaultTheme="system">
+        <ModeToggle/>
+
+        {children}
+        </ThemeProvider>
+        <footer>
+          <p className="text-white">footer</p>
+        </footer>
+      </body>
     </html>
   );
 }
